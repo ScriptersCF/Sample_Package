82346575b2dbc06a1e41093ba8df5e03318f2f84
Josh <me@joshl.io>
2019-11-19 21:24:00 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 -- The module that is accessed when `require("sample")` is used.
 
-local Scripts = script.Parent.Scripts
+local Scripts = require(script.Parent.Scripts)
 
 return {
     Print = Scripts.Print
